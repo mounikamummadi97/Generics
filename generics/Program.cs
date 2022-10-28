5063f66c17df1642d136a78e923be12e82031891
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 12:41:29 +0530

    first commit

---
+++ generics/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using generics;
+using System;
+namespace CollectionProject
+{
+    class Generics
+    {
+        public bool Compare<T>(T a, T b)
+        {
+            if (a.Equals(b))
+
+                return true;
+            return false;
+        }
+        //static void Main()
+        //{
+        //    Generics obj = new Generics();
+        //    bool Result = obj.Compare<double>(20.52f, 10.55);
+        //    Console.WriteLine(Result);
+        //    bool Result2 = obj.Compare<int>(20, 10);
+        //    Console.WriteLine(Result2);
+        //    Console.ReadLine();
+
+        //}
+    }
+}
---
+++ generics/generics2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+   class generics2
+    {
+        public void Add<T>(T a,T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1+ d2);                                                                                                      
+        }
+        public void Sub<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 - d2);
+        }
+        public void Mul<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 * d2);
+        }
+        public void Div<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 / d2);
+        }
+      static public void Main(string[] args)
+        {
+            generics2 obj = new generics2();
+            obj.Add<int>(10, 30);
+            obj.Sub<double>(15.76, 32.46);
+            obj.Mul<float>(10.52f, 30);
+            obj.Div<int>(10, 3);
+        }
+    }
+}
